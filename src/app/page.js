6117e7f6ed79348f265d9a1dece57bb1b5b22093
ivand <ivan.daniel.hdz@gmail.com>
2023-10-18 23:12:58 -0600

    Main screen Tabs

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,88 @@
-import Image from 'next/image'
+'use client'
+
+import * as React from 'react';
 import styles from './page.module.css'
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
+  const [value, setValue] = React.useState(0);
 
-    </main>
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Formulas" {...a11yProps(0)} />
+          <Tab label="Chemicals" {...a11yProps(1)} />
+          <Tab label="Hides Inventory" {...a11yProps(2)} />
+          <Tab label="Imports" {...a11yProps(3)} />
+          <Tab label="Ventas" {...a11yProps(4)} />
+          <Tab label="Maquilas" {...a11yProps(5)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="Search/Run" {...a11yProps(0)} />
+            <Tab label="Log/Report" {...a11yProps(1)} />
+            <Tab label="Engrases" {...a11yProps(2)} />
+          </Tabs>
+        </Box>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        Item Two
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        Item Three
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={3}>
+        Item Four
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={4}>
+        Item Five
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={5}>
+        Item Six
+      </CustomTabPanel>
+    </Box>
   )
 }
